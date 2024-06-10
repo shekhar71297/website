b4ec5e5b9bf46b23c65614b049038c2e9db0b177
shekhar <shekhar.powar@outlook.com>
2024-06-10 16:53:24 +0530

    home page done

---
+++ hematitewebsite/src/common/footer/FooterPage.js
@@ -0,0 +1,153 @@
+
+import React, { useContext, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Col, Container, Row } from 'react-bootstrap'
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin,
+  faYoutube,
+  faWhatsapp,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { WebContext } from "../../App";
+import './footer.css'
+function FooterPage() {
+	const data = useContext(WebContext);
+	const location = useLocation();
+	const socialLinks = [
+    {
+      href: "https://www.facebook.com/hematiteinfotech/",
+      icon: faFacebook,
+      className: "facebook",
+    },
+    {
+      href: "https://www.instagram.com/hematite_infotech?igsh=ZDE3OTNnNzdjNnl4",
+      icon: faInstagram,
+      className: "instagram",
+    },
+    { href: "https://wa.link/he878r", icon: faWhatsapp, className: "whatsapp" },
+    {
+      href: "https://www.linkedin.com/company/hematite-infotech-pvt-ltd/mycompany/",
+      icon: faLinkedin,
+      className: "linkedin",
+    },
+    {
+      href: "http://www.youtube.com/@hematiteinfotech1569",
+      icon: faYoutube,
+      className: "youtube",
+    },
+  ];
+
+  useEffect(() => {
+    if (location.state && location.state.scrollToTop) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [location]);
+	return (
+		<div>
+			<footer className="footer-body">
+				<Container>
+					<Row>
+						<Col md={4}  >
+
+							<div className='about-heading'>
+								<div> </div>
+								<div>
+									<span className="footer-title" >{data?.footer?.sloganheading?.title}</span>
+								</div>
+							</div>
+							<div className="footer-content" >
+								<p>{data?.footer?.slogan}</p>
+							</div>
+
+
+						</Col>
+						<Col md={4} >
+							<div className="site-map-body" >
+								<div className='about-heading'>
+									<div>
+										<span className="footer-title" >{data?.footer?.siteMap?.title}</span>
+									</div>
+								</div>
+								<div className="footer-site-map" >
+									{data?.footer?.siteMap?.items?.map((item, index) => (
+										<p key={index}>
+											<Link
+												className="site-map-links"
+												to={item.link}
+												state={{ scrollToTop: true }}
+											>
+												{/* Display the arrow icon and link text */}
+												<MdOutlineKeyboardDoubleArrowRight /> {item.name}
+											</Link>
+										</p>
+									))}
+								</div>
+							</div>
+						</Col>
+						<Col md={4} >
+							<div >
+								<div className='about-heading'>
+									<div>
+										<span className="footer-title" >{data?.footer?.headOffice?.title}</span>
+									</div>
+									<div className="address-body">
+									<div className="footer-contact">
+										<div>
+											<FaLocationDot className="footer-icon" />
+										</div>
+										<span>{data?.footer?.headOffice?.address}</span>
+									</div>
+
+									<div className="footer-email">
+										<div>
+											<IoMdMail className="footer-icon" />
+										</div>
+										<a href={`mailto:${data?.footer?.headOffice?.email}`}>
+											<span>{data?.footer?.headOffice?.email}</span>
+										</a>
+									</div>
+
+									<div className="footer-phone">
+										<div>
+											<FaPhone className="footer-icon" />
+										</div>
+										<a href={`tel:${data?.footer?.headOffice?.phone}`}>
+											<span>{data?.footer?.headOffice?.phone}</span>
+										</a>
+									</div>
+									</div>
+								</div>
+							</div>
+						</Col>
+					</Row>
+						
+					
+				</Container>
+			</footer>
+			<div className="copyright-body" >
+					<Container>
+						<div className="footer-copyright" >
+						<span>{data?.footer?.copyright}</span>
+				
+						<span>{data?.footer?.subheading}</span>
+						</div>
+						</Container>
+						</div>
+						<div className="icon-bar">
+        {socialLinks.map((link) => (
+          <a key={link.href} href={link.href} className={link.className}>
+            <FontAwesomeIcon icon={link.icon} />
+          </a>
+        ))}
+      </div>
+		</div>
+	)
+}
+
+export default FooterPage
--- hematitewebsite/src/pages/home/HomePage.js
+++ hematitewebsite/src/pages/home/HomePage.js
@@ -1,10 +1,209 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import Slider from "react-slick";
+import ScrollTrigger from "react-scroll-trigger";
 import NavigationBar from '../../common/navbar/NavigationBar'
+import { WebContext } from '../../App';
+import './home.css'
+import { Link } from 'react-router-dom';
+import CountUp from "react-countup";
+import FooterPage from '../../common/footer/FooterPage';
+import BackToTop from '../../common/backTotop/BackToTop';
 
 function HomePage() {
+  const data = useContext(WebContext);
+  const [counterOn, setCounterOn] = useState(false);
+  let settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+  if (document.body.clientWidth < 500) {
+    settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: 2000,
+      cssEase: "linear",
+    };
+  }
+
+  if (document.body.clientWidth > 665 && document.body.clientWidth < 750) {
+    settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: 2000,
+      cssEase: "linear",
+    };
+  }
+  if (document.body.clientWidth >751 && document.body.clientWidth < 1026) {
+    settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: 2000,
+      cssEase: "linear",
+    };
+  }
   return (
     <div>
-      <NavigationBar/>
+      <header>
+        <NavigationBar />
+      </header>
+      <div className='home-body' >
+        <Container>
+          {data?.HomePage?.map((val, index) => (
+            <Row>
+              <Col sm={6} md={6} >
+                <div className='home-content'>
+                  <p className='home-title' >{val.title}</p>
+                  <div className='home-tagline' >
+                    <span className='home-tagline1'>{val.tagline1}</span>
+                    <span className='home-tagline2' >{val.tagline2}</span>
+                    <span className='home-tagline2' >{val.tagline3}</span>
+                  </div>
+                  <div className="enroll-button">
+                    <Button size="lg" className="btn">
+                      ENROLL NOW
+                    </Button>
+                  </div>
+                </div>
+              </Col>
+              <Col sm={6} md={6} >
+                <div className="home-banner-image">
+                  <img
+                    src={val.bannerImgurl}
+                    alt="HomeImage"
+                    className="home-image"
+                  />
+                </div>
+              </Col>
+            </Row>
+          ))}
+        </Container>
+        </div>
+        {/*---------------------------------------------------about us section----------------------------------------*/}
+        <Container>
+        <div className='about-body'>
+          {data?.homepageabout?.map((val, index) => (
+            <Row>
+              <Col md={6}>
+                <div className="aboutus-image">
+                  <img
+                    src={val.aboutImageurl}
+                    alt="homeaboutimage"
+                    className="home-about-image"
+                  />
+                </div>
+              </Col>
+              <Col md={6}>
+                <div className='about-section'>
+                  <div>
+                    <span className="pre-title">About</span>
+                    <h3 className="about-title">{val.title}</h3>
+                  </div>
+                  <div className="description-wrapper">
+                    <p className="description">{val.description1}</p>
+                  </div>
+                  <div className="description-wrapper">
+                    <p className="description">{val.description2}</p>
+                  </div>
+                  <div className="aboutus-btn">
+                    <Link to='/about' className="btn-about">KNOW MORE</Link>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          ))}
+          </div>
+        </Container>
+    {/* -----------------------------------------------Slider-section-------------------------------------------- */}
+    <div className='slider-section' >
+      <Container>
+        <Row>
+          <Col>
+          <div className="slider-container">
+              <p className="slider-container-main-heading">OUR PLACED STUDENTS</p>
+              <Slider {...settings}>
+                {/* This is the Slider component, which takes settings as props */}
+                {data?.placedstudents?.map((src) => (
+                  <div className="slider-image" key={src.id}>
+                    <img
+                      className="image"
+                      src={`${src.img}`}
+                      alt={`Slide ${src.id}`}
+                    />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+    {/*----------------------------------------------------couter bar---------------------------------------------*/}
+    <div className='counter-section' >
+        <Container>
+          <Row>
+          {data?.Counters?.map((counter, index) => (
+            <Col sm={6} md={3} >
+              <Card className="counter-card" key={index}>
+              <Card.Body className="counter-card-body">
+                {counter.icon && (
+                  <img
+                    src={counter.icon}
+                    className="counter-logo"
+                    alt="icon"
+                  />
+                )}
+                <Card.Title className="counter-title" >{counter.title}</Card.Title>
+                <Card.Text
+                  className="counter-cardtext"
+                  style={{ textAlign: "center" }}
+                >
+                  <ScrollTrigger
+                    onEnter={() => setCounterOn(true)}
+                    onExit={() => setCounterOn(false)}
+                  >
+                    <div>
+                      <p>
+                        {counterOn && (
+                          <CountUp
+                            start={0}
+                            end={counter.value}
+                            duration={2}
+                            delay={0}
+                          />
+                        )}
+                        +
+                      </p>
+                    </div>
+                  </ScrollTrigger>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </Col>
+          ))}
+          </Row>
+        </Container>
+    </div>
+    <BackToTop/>
+    <FooterPage/>
     </div>
   )
 }
