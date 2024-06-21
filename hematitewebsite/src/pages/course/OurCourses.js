ed6aed8e9b6a91f13e89727a76f3dbf114fef8b4
Vanshi <vsatdeve7@gmail.com>
2024-06-21 16:00:18 +0530

    change course Details format and add 9 course details

--- hematitewebsite/src/pages/course/OurCourses.js
+++ hematitewebsite/src/pages/course/OurCourses.js
@@ -35,8 +35,8 @@ function OurCourses() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     
     
     responsive: [
