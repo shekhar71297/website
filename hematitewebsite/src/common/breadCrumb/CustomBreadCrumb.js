5b301af68a7bc22844bcca290bfa531761ff5228
shekhar <shekhar.powar@outlook.com>
2024-09-19 18:22:59 +0530

    "Updated db.json, CustomBreadCrumb.js, OurCourses.js, and HomePage.js files with minor changes to content and formatting."

--- hematitewebsite/src/common/breadCrumb/CustomBreadCrumb.js
+++ hematitewebsite/src/common/breadCrumb/CustomBreadCrumb.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Breadcrumb } from "react-bootstrap";
-// import './BreadCrumb.css'
-import './breadCrumb.css'
+import './breadCrumb.css';
 import { Link } from 'react-router-dom';
+
 function CustomBreadCrumb({ pageTitle }) {
   return (
-
     <div className="header">
       <div className='container'>
-        <span className='PageName' >{pageTitle}</span>
+        <span className='PageName'>{pageTitle}</span>
         <Breadcrumb className="custom-breadcrumb">
-          <Breadcrumb.Item>
-            <Link className="breadcrumb-link" to='/'>Home</Link>
+          <Breadcrumb.Item  linkAs={Link} linkProps={{ to: '/' }}>
+            <span className='breadcrumb-link' >Home</span>
           </Breadcrumb.Item>
-          <Breadcrumb.Item className="active-item" active>
+          <Breadcrumb.Item active>
             {pageTitle}
           </Breadcrumb.Item>
         </Breadcrumb>
